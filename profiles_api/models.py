5b2fe11193347f7d5807249d48108ffc1a9dd397
mayank <2018eeb1164@iitrpr.ac.in>
2024-02-09 11:32:30 +0530

    first api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, 
+                                        PermissionsMixin, 
+                                        BaseUserManager
+                                        )
 
-# Create your models here.
+
+#base user model customized
+class UserModel(BaseUserManager):
+    """Manager for user profile"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """create and save new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+        
+
+# custom user model
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """DB model for user in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    objects = UserModel()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """get full name"""
+        return self.name
+    
+    def get_short_name(self):
+        return self.name
+    
+    def __str__(self):
+        """returning representation of user"""
+        return self.email
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Api view test"""
+
+    def get(self, request, format=None):
+        """return a list of APIView features"""
+        an_api = [
+            "HTTP methods (get, put, post, pastch, delete)",
+            "Is similar to django view",
+            "gives u the most controll over your api view"
+        ]
+        return Response({'masg':"Hello", 'an_apiView':an_api})
