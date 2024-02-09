8a7bf6d93d17a8cef8b0d6fc5fede6cf21fa33c1
mayank <2018eeb1164@iitrpr.ac.in>
2024-02-09 12:16:47 +0530

    api class views crud

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from profiles_api import serializers
+# from serializers import HelloSerializer
+from rest_framework import status
 
 
 class HelloApiView(APIView):
     """Api view test"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """return a list of APIView features"""
@@ -13,3 +17,27 @@ class HelloApiView(APIView):
             "gives u the most controll over your api view"
         ]
         return Response({'masg':"Hello", 'an_apiView':an_api})
+    
+    def post(self, request):
+        """create hello msg with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            msg = f"Hello {name}"
+            return Response({'msg': msg})
+        
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+    def put(self, request, pk=None):
+        return Response({"msg":"put"})
+    
+    def patch(self, request, pk=None):
+        return Response({"msg":"patch"})
+    
+    def delete(self, request, pk=None):
+        return Response({"msg":"delete"})
